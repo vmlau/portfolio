13aaa6e050436971b641290bdaf0cd103d43e94c
vmlau <valau@ucsd.edu>
2025-10-15 20:18:50 -0700

    finished step 2

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+//currentLink.classList.add('current');
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
+}
+//currentLink?.classList.add('current');
+console.log(currentLink)
