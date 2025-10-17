cccce7664ff5f31c680ac5054c252ddc6dedbdfe
vmlau <valau@ucsd.edu>
2025-10-16 17:25:38 -0700

    4.3

--- global.js
+++ global.js
@@ -4,14 +4,54 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
-//currentLink.classList.add('current');
-if (currentLink) {
-  // or if (currentLink !== undefined)
-  currentLink.classList.add('current');
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// //currentLink.classList.add('current');s
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
+// //currentLink?.classList.add('current');
+// console.log(currentLink)
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/vmlau', title: 'GitHub' },
+];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+for (let p of pages) {
+  let url = p.url;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  let title = p.title;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
 }
-//currentLink?.classList.add('current');
-console.log(currentLink)
+  if (a.host !== location.host && a.pathname === location.pathname) {
+    a.target = "_blank";
+}
+}
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="light dark">Automatic</option>
+		</select>
+	</label>`,
+);
