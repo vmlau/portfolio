304c66053e4f975ef52c991b43d84f43d9599168
vmlau <valau@ucsd.edu>
2025-10-17 16:59:20 -0700

    Update global.js

--- global.js
+++ global.js
@@ -27,7 +27,7 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  
-  : "/portfolio/";         
+  : "/website/portfolio/";         
 for (let p of pages) {
   let url = p.url;
   url = !url.startsWith('http') ? BASE_PATH + url : url;
